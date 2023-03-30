23b18f67591c34169a9dfc80bf97468932841497
Jayvee Milana <jmmilana12@gmail.com>
2023-03-30 14:16:45 -0500

    get name functions

--- assets/script/index.js
+++ assets/script/index.js
@@ -15,8 +15,17 @@ const settings = document.querySelector(".settings");
 const settingsModule = document.querySelector(".settings-module");
 const save = document.querySelector(".save");
 
+const date = new Date();
+date.setSeconds(date.getSeconds() + 15);
+
 accept.addEventListener('click', function() {
   cookiesModule.classList.add('hidden');
+
+  setCookie('Browser', `${getBrowserName}`, {'expires': date});
+  setCookie('Operating System', `${getOSName}`, {'expires': date});
+
+  console.log(getCookie('Brower'));
+  console.log(getCookie('Operating System'));
 })
 
 settings.addEventListener('click', function() {
@@ -27,3 +36,80 @@ settings.addEventListener('click', function() {
 save.addEventListener('click', function() {
   settingsModule.classList.add('hidden');
 })
+
+function getBrowserName() {          
+  let userAgent = navigator.userAgent;
+  let browserName;
+    
+  if(userAgent.match(/chrome|chromium|crios/i)){
+    browserName = "chrome";
+  }else if(userAgent.match(/firefox|fxios/i)){
+    browserName = "firefox";
+  }  else if(userAgent.match(/safari/i)){
+    browserName = "safari";
+  }else if(userAgent.match(/opr\//i)){
+    browserName = "opera";
+  } else if(userAgent.match(/edg/i)){
+    browserName = "edge";
+  }else{
+    browserName="No browser detection";
+  }
+  return browserName
+}
+
+function getOSName() {
+  let userAgent = window.navigator.userAgent,
+      platform = window.navigator.platform,
+      macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+      windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+      iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+      os = null;
+  
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    os = 'Mac OS';
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+    os = 'iOS';
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    os = 'Windows';
+  } else if (/Android/.test(userAgent)) {
+    os = 'Android';
+  } else if (!os && /Linux/.test(platform)) {
+    os = 'Linux';
+  }
+  return os;
+}
+
+function setCookie(name, value, options = {}) {
+  options = {
+    path: '/',
+    SameSite: 'Lax',
+    ...options
+  };
+
+  const keys = Object.keys(options);
+  const values = Object.values(options);
+
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString();
+  }
+
+  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+  for (let i = 0; i < keys.length; i++) {
+    updatedCookie += `; ${keys[i]}=${values[i]}`;
+  }
+
+  document.cookie = updatedCookie;
+}
+
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function deleteCookie(name) {
+  setCookie(name, '', {'max-age': 15});
+}
