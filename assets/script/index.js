7263ec52410bb73e70e3205873a9deba93e3178e
Jayvee Milana <jmmilana12@gmail.com>
2023-03-30 11:54:33 -0500

    buttons

--- assets/script/index.js
+++ assets/script/index.js
@@ -8,3 +8,22 @@ Cookies
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 'use strict';
+
+const accept = document.querySelector(".accept");
+const cookiesModule = document.querySelector(".cookies-module");
+const settings = document.querySelector(".settings");
+const settingsModule = document.querySelector(".settings-module");
+const save = document.querySelector(".save");
+
+accept.addEventListener('click', function() {
+  cookiesModule.classList.add('hidden');
+})
+
+settings.addEventListener('click', function() {
+  settingsModule.classList.remove('hidden');
+  cookiesModule.classList.add('hidden');
+})
+
+save.addEventListener('click', function() {
+  settingsModule.classList.add('hidden');
+})
