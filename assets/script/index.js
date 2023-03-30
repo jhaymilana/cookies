548da2399cd98fa1626d70a4060192b79156d546
Jayvee Milana <jmmilana12@gmail.com>
2023-03-30 14:37:14 -0500

    cookie functions

--- assets/script/index.js
+++ assets/script/index.js
@@ -9,11 +9,20 @@ Cookies
 
 'use strict';
 
+// Buttons
 const accept = document.querySelector(".accept");
-const cookiesModule = document.querySelector(".cookies-module");
-const settings = document.querySelector(".settings");
-const settingsModule = document.querySelector(".settings-module");
 const save = document.querySelector(".save");
+const settings = document.querySelector(".settings");
+
+// Checkboxes
+const browserBox = document.querySelector(".browser-box");
+const osBox = document.querySelector(".os-box");
+const widthBox = document.querySelector(".width-box");
+const heightBox = document.querySelector(".height-box");
+
+const cookiesModule = document.querySelector(".cookies-module");
+const settingsModule = document.querySelector(".settings-module");
+
 
 const date = new Date();
 date.setSeconds(date.getSeconds() + 15);
@@ -23,9 +32,19 @@ accept.addEventListener('click', function() {
 
   setCookie('Browser', `${getBrowserName}`, {'expires': date});
   setCookie('Operating System', `${getOSName}`, {'expires': date});
+  setCookie('Window Width', `${window.innerWidth}`, {'expires': date});
+  setCookie('Window Height', `${window.innerHeight}`, {'expires': date});
 
-  console.log(getCookie('Brower'));
-  console.log(getCookie('Operating System'));
+  console.log(`Browser: ${getBrowserName()}`);
+  console.log(`Operating System: ${getOSName()}`);
+  console.log(`Window Width: ${window.innerWidth}`);
+  console.log(`Window Height: ${window.innerHeight}`);
+/*
+  console.log(`Browser: ${getCookie('Browser)}`);
+  console.log(`Operating System: ${getCookie('OperatingSystem')}`);
+  console.log(`Window Width: ${getCookie('Window Width')}`);
+  console.log(`Window Height: ${getCookie('Window Height')}`);
+*/
 })
 
 settings.addEventListener('click', function() {
@@ -35,6 +54,26 @@ settings.addEventListener('click', function() {
 
 save.addEventListener('click', function() {
   settingsModule.classList.add('hidden');
+
+  if (browserBox.checked === true) {
+    console.log(`Browser: ${getBrowserName()}`);
+    // console.log(`Browser: ${getCookie('Browser)}`);
+  }
+
+  if (osBox.checked === true) {
+    console.log(`Operating System: ${getOSName()}`);
+    // console.log(`Operating System: ${getCookie('OperatingSystem')}`);
+  }
+
+  if (widthBox.checked === true) {
+    console.log(`Window Width: ${window.innerWidth}`);
+    // console.log(`Window Width: ${getCookie('Window Width')}`);
+  }
+
+  if (heightBox.checked === true) {
+    console.log(`Window Height: ${window.innerHeight}`);
+    // console.log(`Window Height: ${getCookie('Window Height')}`);
+  }
 })
 
 function getBrowserName() {          
